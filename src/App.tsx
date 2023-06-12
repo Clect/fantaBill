93f806ee7c704645f735869dbc4035c994734b78
Edward Li <clect@foxmail.com>
2023-06-12 19:10:59 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="fanta-bill-container">
+      FantaBill
+    </div>
+  );
+}
+
+export default App;
